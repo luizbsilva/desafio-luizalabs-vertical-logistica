73df8235885c7e22b6b36fab0a59ab27fa85ceb6
José Luiz <luizbsilva@gmail.com>
2024-07-08 15:03:22 -0300

    chore: criação do projeto

---
+++ process-order-file/src/main/java/br/com/kayros/processor/ProcessOrderFileApplication.java
@@ -0,0 +1,13 @@
+package br.com.kayros.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessOrderFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessOrderFileApplication.class, args);
+	}
+
+}
---
+++ process-order-file/src/test/java/br/com/kayros/processor/ProcessOrderFileApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kayros.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessOrderFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
